fe711a55aa66c449b4e95bc846ba04596fa12fce
FedericoMelograna <melogranafederico@gmail.com>
2024-04-23 09:17:15 +0200

    Update MBMDR_wrapper.R
    Update MBMDR_wrapper.R to  align with the previous pull request

--- Code/mbmdrc_wrapper.R
+++ Code/mbmdrc_wrapper.R
@@ -8,13 +8,13 @@ download.file("https://raw.githubusercontent.com/FedericoMelograna/MBMDRc_R/main
 source("MBMDRc_script.R")
 
 
-Wrapper_MBMDRc = function(p_threshold, is_0_considered, data_path, result_path, model, output, data, d_level){
+Wrapper_MBMDRc = function(p_threshold, is_0_considered, model, chisq, d_level, seed = 7){
   d_level = as.numeric(d_level)
   if (d_level == 1){
-    Calc_MBMDR_1d(p_threshold, is_0_considered, data_path, result_path, model, output, data)
+    Calc_MBMDR_1d(p_threshold, is_0_considered, model, chisq, seed)
     print("MBMDRc-1d successfully run")
   } else if (d_level == 2) {
-    Calc_MBMDR_2d(p_threshold, is_0_considered, data_path, result_path, model, output, data)
+    Calc_MBMDR_2d(p_threshold, is_0_considered, model, chisq, seed)
     print("MBMDRc-2d successfully run")
   }
 }
